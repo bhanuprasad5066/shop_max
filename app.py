dcb519567da817777f3f486e4741ab92c46ea6ee
Bhanu <bhanubhanu62818@gmail.com>
2026-03-08 13:03:12 +0530

    Updated app for deployment

--- app.py
+++ app.py
@@ -1,8 +1,3 @@
-import os
-from shopmax import create_app
-
+from shopmax import create_app 
 app = create_app()
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    app.run(host="0.0.0.0", port=port)
+if __name__ == "__main__": app.run(debug=True)
